4815c93509ec72a75902187a7e9a84aa0d124e44
Hyeonil Choi <tt2t2am1118@naver.com>
2022-02-14 02:48:59 +0900

    Developing, 2022-02-14 월, 02:48.

--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -1,13 +1,20 @@
 public class Computer {
 
+    private Operater opt;
+    Server a = new Server();
+
     Computer(){
         Ram ram01= new Ram();
         CPU inoCpu = new CPU();
         InputTry iT = new InputTry(1);
         TurnOn com1 = new TurnOn(ram01,inoCpu,iT);
+
     }
 
     public static void main(){
         new com1();
+        Operater opt = new Operater("Hello.");
+        a. ... //오퍼레이터와 통신, 일을 할 것인지 물어보기
+        //한다면... com1()을 줌. 어떤 일에 사용된다는 예로...
     }
 }
